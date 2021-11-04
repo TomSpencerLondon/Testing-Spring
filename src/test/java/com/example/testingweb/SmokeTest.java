17736e8d22de3b0b393258c609d132161bc3a809
Tom <thomas.spencer@codurance.com>
2021-11-04 06:30:20 +0000

    Add test for full application context

--- src/test/java/com/example/testingweb/SmokeTest.java
+++ src/test/java/com/example/testingweb/SmokeTest.java
@@ -7,9 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestingWebApplicationTests {
-	@Test
-	public void contextLoads(){
+public class SmokeTest {
+  @Autowired
+  private HomeController controller;
 
-	}
+  @Test
+  void contextLoads() throws Exception {
+    assertThat(controller).isNotNull();
+  }
+
 }
